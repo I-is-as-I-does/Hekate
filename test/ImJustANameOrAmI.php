86f339a450671209d0b325a3748e6441c4082d31
I-is-as-I-does <66915011+I-is-as-I-does@users.noreply.github.com>
2021-07-12 18:30:10 +0200

    test folder move

--- test/ImJustANameOrAmI.php
+++ test/ImJustANameOrAmI.php
@@ -1,5 +1,5 @@
 <?php
-
+/* This file is part of Hekate | SSITU | (c) 2021 I-is-as-I-does | MIT License */
 namespace SSITU\Hekate\Test;
 
 class ImJustANameOrAmI
--- test/script.php
+++ test/script.php
@@ -1,8 +1,9 @@
 <?php
+/* This file is part of Hekate | SSITU | (c) 2021 I-is-as-I-does | MIT License */
 
 use SSITU\Hekate\Test\TestRunner;
 
-$pathToAutoload = dirname(__DIR__, 4) . '/app/vendor/autoload.php'; # EDIT
+$pathToAutoload = dirname(__DIR__, 3) . '/app/vendor/autoload.php'; # EDIT
 require_once $pathToAutoload;
 
 require_once __DIR__ . '/globalScope.php';
